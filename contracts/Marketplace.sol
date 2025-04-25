4a3e9c46b09668b4d476f0425c03bf23902dc3c9
NSuryansh <nagar.suryansh@gmail.com>
2025-04-25 22:38:56 +0530

    market place added in backend

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IEventTicket {
+    function transferFrom(address from, address to, uint256 tokenId) external;
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function isTicketValid(uint tokenId) external view returns (bool);
+}
+
+contract TicketMarketplace {
+    struct Listing {
+        address seller;
+        uint price;
+    }
+
+    address public eventTicketAddress;
+    mapping(uint => Listing) public listings;
+
+    constructor(address _eventTicketAddress) {
+        eventTicketAddress = _eventTicketAddress;
+    }
+
+    function listTicket(uint tokenId, uint price) external {
+        IEventTicket ticket = IEventTicket(eventTicketAddress);
+
+        require(ticket.ownerOf(tokenId) == msg.sender, "Not the ticket owner");
+        require(ticket.isTicketValid(tokenId), "Ticket is invalid");
+        require(price > 0, "Price must be greater than 0");
+
+        listings[tokenId] = Listing(msg.sender, price);
+    }
+
+    function cancelListing(uint tokenId) external {
+        require(listings[tokenId].seller == msg.sender, "Not the seller");
+        delete listings[tokenId];
+    }
+
+    function buyTicket(uint tokenId) external payable {
+        Listing memory item = listings[tokenId];
+        require(item.price > 0, "Ticket not for sale");
+        require(msg.value == item.price, "Incorrect payment");
+
+        // Transfer the ticket
+        IEventTicket(eventTicketAddress).transferFrom(item.seller, msg.sender, tokenId);
+
+        // Pay the seller
+        payable(item.seller).transfer(msg.value);
+
+        // Remove listing
+        delete listings[tokenId];
+    }
+
+    function getListing(uint tokenId) external view returns (address seller, uint price) {
+        Listing memory item = listings[tokenId];
+        return (item.seller, item.price);
+    }
+}
