affcce1f9b661d2b27ff800d4fa230469bb70312
Manan Nigam <manannigam07@gmail.com>
2025-04-25 18:07:15 +0530

    ethersService.js functions implememented

--- src/ethers/ethersService.js
+++ src/ethers/ethersService.js
@@ -1,13 +1,66 @@
 const { formatEther, JsonRpcProvider } = require('ethers');
+import EventTicketABI from "../../build/contracts/EventTicket.json";
 
-// Create a provider using Infura
+const CONTRACT_ADDRESS = '0xDC724594e639F5082104073F72fefb3B56e12F90';
 const INFURA_ID = 'd404f2d478314b50b2498dcfa1652902';
-const provider = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
 
-const CONTRACT_ADDRESS = '0xa57F3911D1f21E3033bA1D4CDc2987a4B280658D';
+// Create a provider and contract instance
+const readProvider = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+const readContract = new ethers.Contract(CONTRACT_ADDRESS, EventTicketABI.abi, readProvider);
+
+// Connect to wwallet and return writeable contract instance
+export const getWriteableContract = async () => {
+    if (!window.ethereum) throw new Error("MetaMask not installed");
+    const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+    await web3Provider.send("eth_requestAccounts", []);
+    const signer = web3Provider.getSigner();
+    return new ethers.Contract(CONTRACT_ADDRESS, EventTicketABI.abi, signer);
+};
+
+// Create event
+export const createEvent = async (eventName, eventDate, eventPrice, totalTickets) => {
+    const contract = await getWriteableContract();
+    const tx = await contract.createEvent(eventName, eventDate, eventPrice, totalTickets);
+    await tx.wait();
+    return tx;
+}
+
+// Buy Ticket
+export const buyTicket = async (eventId, ticketURI , priceInWei) => {
+    const contract = await getWriteableContract();
+    const tx = await contract.buyTicket(eventId, ticketURI, { value: priceInWei });
+    await tx.wait();
+    return tx;
+}
+
+// Fetch events
+export const getEvent = async(eventId) => {
+    return await readContract.events(eventId);
+}
+
+// Check ticket availability
+export const isTicketAvailable = async (tokenId) => {
+    return await readContract.isTicketValid(tokenId);
+}
+
+// Resell ticket
+export const resellTicket = async (buyer, tokenId) => {
+    const contract = await getWriteContract();
+    const tx = await contract.resellTicket(to, tokenId);
+    await tx.wait();
+    return tx;
+}
+
+// Invalidate ticket (Owner Only)
+export const invalidateTicket = async (tokenId) => {
+    const contract = await getWriteContract();
+    const tx = await contract.invalidateTicket(tokenId);
+    await tx.wait();
+    return tx;
+};
 
 const main = async () => {
-    const balance = await provider.getBalance('0xd7830925daf4a323c3781a57c4aa57a512838743');
+    const balance = await readProvider.getBalance('0xd7830925daf4a323c3781a57c4aa57a512838743');
     console.log("Balance:", formatEther(balance), "ETH");
 };
 
