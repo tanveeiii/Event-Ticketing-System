b0c61593604489b65759cc62451729a0e46f927f
Tanvi Agarwal <agarwaltanvi1507@gmail.com>
2025-04-25 23:26:11 +0530

    Adding marketplace contract

--- migrations/2_deploy_event.js
+++ migrations/2_deploy_event.js
@@ -1,5 +1,9 @@
 const EventTicket = artifacts.require("EventTicket");
+const Marketplace = artifacts.require("TicketMarketplace");
 
-module.exports = function (deployer) {
-  deployer.deploy(EventTicket);
+module.exports = async function (deployer) {
+  await deployer.deploy(EventTicket);
+  const eventTicketInstance = await EventTicket.deployed();
+
+  await deployer.deploy(Marketplace, eventTicketInstance.address);
 };
