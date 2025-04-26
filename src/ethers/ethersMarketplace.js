86ebf52e479dac5ba7525a68367d637b2d66c458
Tanvi Agarwal <agarwaltanvi1507@gmail.com>
2025-04-26 20:30:21 +0530

    marketplace fixes

--- src/ethers/ethersMarketplace.js
+++ src/ethers/ethersMarketplace.js
@@ -1,19 +1,26 @@
 import { ethers, JsonRpcProvider } from 'ethers';
 import TicketMarketplaceABI from "../../build/contracts/TicketMarketplace.json";
 
-const TICKETMARKETPLACE_ADDRESS = '0x4C96C48bAD034c4FDC09ED5E4c1665382A34556E';
-const INFURA_ID = 'd404f2d478314b50b2498dcfa1652902';
+const TICKETMARKETPLACE_ADDRESS = import.meta.env.VITE_TICKET_MARKETPLACE_ADDRESS
+// const INFURA_ID = 'd404f2d478314b50b2498dcfa1652902';
 
-const marketplaceProvider = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
-const marketplaceContract = new ethers.Contract(TICKETMARKETPLACE_ADDRESS, TicketMarketplaceABI.abi, marketplaceProvider);
+// const marketplaceProvider = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+// const marketplaceContract = new ethers.Contract(TICKETMARKETPLACE_ADDRESS, TicketMarketplaceABI.abi, marketplaceProvider);
 
+const provider = new JsonRpcProvider("http://127.0.0.1:7545")
+const eventContract = new ethers.Contract(TICKETMARKETPLACE_ADDRESS, TicketMarketplaceABI.abi,provider);
+
+// Connect to wallet and return writeable contract instance
 export const getMarketplaceContract = async () => {
-    if (!window.ethereum) throw new Error("MetaMask not installed");
-    const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
-    await web3Provider.send("eth_requestAccounts", []);
-    const signer = web3Provider.getSigner();
-    return new ethers.Contract(TICKETMARKETPLACE_ADDRESS, TicketMarketplaceABI.abi, signer);
+  if (window.ethereum == null) throw new Error("MetaMask not installed");
+  const web3Provider = new ethers.BrowserProvider(window.ethereum);
+  console.log(web3Provider)
+  await web3Provider.send("eth_requestAccounts", []);
+  const signer = await web3Provider.getSigner();
+  console.log(signer)
+  return new ethers.Contract(TICKETMARKETPLACE_ADDRESS, TicketMarketplaceABI.abi, signer);
 };
+
 
 // List tickets
 export const listTicket = async (tokenId, priceInWei) => {
